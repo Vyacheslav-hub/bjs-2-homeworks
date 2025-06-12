0e91f32a7239696a021ed45fe4dbe744b6bdf500
Vyacheslav <slavik.kurganov.26@mail.ru>
2025-06-12 18:26:33 +0300

    Решил заново

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,29 +1,34 @@
 "use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  let d = b ** 2 - 4 * a * c;
-  if (d === 0) {
-    arr =  [-b / ( 2 * a)];
-  } else if (d > 0) {
-    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
-  }
-  return arr;
+
+function solveEquation(a, b , c) {
+    let d = b ** 2 - 4 * a * c;
+
+    if (d < 0) {
+        return [];
+    }
+
+    if (d === 0) {
+        let root = -b / (2 * a)
+        return [root];
+    }
+
+    if (d > 0) {
+        const root1 = (-b + Math.sqrt(d) ) / (2 * a);
+        const root2 = (-b - Math.sqrt(d) ) / (2 * a);
+        return [root1, root2];
+
+    }
+
 }
-
-
-
+(solveEquation(1, 5, 4));
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  debugger
-  let amountMonth = (percent / 100) / 12;
+    const interestRate = percent / 100 / 12;
+    const principal = amount - contribution;
+    if (principal <= 0) return 0;
 
-  let bodyCredit = amount - contribution;
-
-  let payment = bodyCredit * (amountMonth + (amountMonth / (((1 + amountMonth) ** countMonths) - 1)));
-  
-  const sum = payment * countMonths;
-  if (sum > 0) {
-  return +sum.toFixed(2); 
-} else return +sum;
-
+    const monthlyPayment = principal * (interestRate + (interestRate / (((1 + interestRate) ** countMonths) - 1)));
+    const totalPayment = +(monthlyPayment * countMonths).toFixed(2);
+    return totalPayment;
 }
+console.log(calculateTotalMortgage(10, 0, 50000, 12));
