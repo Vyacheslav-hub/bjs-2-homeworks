352befeb53043d58e6b5d8a0ba951cd1de063bea
Vyacheslav <slavik.kurganov.26@mail.ru>
2025-09-16 23:22:26 +0300

    Преобразил

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,122 +1,166 @@
-class PrintEditionItem  {
-    constructor (name, releaseDate, pagesCount) {
+class PrintEditionItem {
+    #state
+    constructor(name, releaseDate, pagesCount) {
         this.name = name;
         this.releaseDate = releaseDate;
         this.pagesCount = pagesCount;
-        this.state = 100;
+        this.#state = 100;
         this.type = null;
-};
+    }
 
-fix() { 
-    this.state *= 1.5;
-};
+    get state () {
+        return this.#state;
+    }
 
-set state (newState) {
-    if (newState < 0) {
-        this._state = 0;
-    } else if (newState > 100) {
-        this._state = 100;
-    } else this._state = newState;
+    set state (value) {
+        if (value < 0) {
+            this.#state = 0;
+        }else if (value > 100) {
+            this.#state = 100;
+        }else this.#state = value
+
+    }
+
+    fix() {
+        this.state *= 1.5;
+    }
+
 }
 
-get state () {
-    return this._state;
-};
-};
-
-
 class Magazine extends PrintEditionItem {
-    constructor (name, releaseDate, pagesCount) {
-        super(name,releaseDate, pagesCount);
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
         this.type = 'magazine';
-    };
-};
+    }
+}
 
 class Book extends PrintEditionItem {
-    constructor (author, name, releaseDate, pagesCount) {
-        super(name,releaseDate, pagesCount);
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
         this.author = author;
         this.type = 'book';
-    };
-};
+    }
+}
 
 class NovelBook extends Book {
-    constructor (author, name, releaseDate, pagesCount) {
-        super(author, name,releaseDate, pagesCount);
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
         this.type = 'novel';
-    };
-};
+    }
+}
 
 class FantasticBook extends Book {
-    constructor (author, name, releaseDate, pagesCount) {
+    constructor(author, name, releaseDate, pagesCount) {
         super(author, name, releaseDate, pagesCount);
         this.type = 'fantastic';
-    };
-};
+    }
+}
 
 class DetectiveBook extends Book {
-    constructor (author, name, releaseDate, pagesCount) {
+    constructor(author, name, releaseDate, pagesCount) {
         super(author, name, releaseDate, pagesCount);
         this.type = 'detective';
-    };
-};
-
-
+    }
+}
+const sherlock = new PrintEditionItem(
+    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+    2019,
+    1008
+);
 
 class Library {
-    constructor (name) {
-    this.name = name;
-    this.books = [];
-    };
+    constructor(name) {
+        this.name = String(name);
+        this.books = [];
+    }
 
-addBook(book) {
-    if (book.state > 30) {
-    this.books.push(book);
-    };
-};
+    addBook(book) {
+        if (book.state > 30) {
+            this.books.push(book);
+        }
+    }
 
-findBookBy(type, value) {
-    return this.books.find(book => book[type] == value) || null;
-};
+    findBookBy(type, value) {
+       return this.books.find(book => book[type] === value) || null;
+    }
 
-giveBookByName(bookName) {
-    if (this.books.some(item => item.name === bookName)) {
-        const booking = this.books.find(items => items.name === bookName);
-        this.books.splice(this.books.indexOf(booking, 1));
-        return booking;
-    } else return null;
-};
-};   
+    giveBookByName(bookName) {
+        const book = this.books.find(book => book.name === bookName);
+        if (!book) {
+            return null;
+        }
+        this.books = this.books.filter(b => b !== book);
+        return book;
+    }
+}
 
+const library = new Library("Библиотека имени Ленина");
 
+library.addBook(
+    new DetectiveBook(
+        "Артур Конан Дойл",
+        "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+        2019,
+        1008
+    )
+);
+library.addBook(
+    new FantasticBook(
+        "Аркадий и Борис Стругацкие",
+        "Пикник на обочине",
+        1972,
+        168
+    )
+);
+
+library.addBook(
+    new DetectiveBook(
+        "Аркадий",
+        "Пикник",
+        1919,
+        200
+    )
+);
 
 class Student {
     constructor(name) {
         this.name = name;
         this.marks = {};
-    };
+    }
 
     addMark(mark, subject) {
         if (mark >= 2 && mark <= 5) {
-            if(!this.marks?.[subject]) {
-                this.marks[subject] = [];
-            }
-            this.marks[subject].push(mark);
+           if (!this.marks?.[subject]) {
+               this.marks[subject] = [];
+           }
+           this.marks[subject].push(mark);
         }
-    };
+    }
 
     getAverageBySubject(subject) {
-        if (this.marks?.[subject]) {
-            return this.marks[subject].reduce((a,b) => a + b) / this.marks[subject].length;
-        }else return 0;
-    };
+        if (!this.marks?.[subject]) {
+            return 0;
+        }
+       return this.marks[subject].reduce((sum, n) => sum + n) / this.marks[subject].length
+    }
 
     getAverage() {
-        let sum = 0;
-        let keys = Object.keys(this.marks);
-        keys.forEach(item => sum += this.getAverageBySubject(item));
-        return sum / keys.length || 0;
-    };
-};
-
-
+        const subjects = Object.keys(this.marks);
+        const total = subjects.reduce((sum, subject) => {
+            return sum + this.getAverageBySubject(subject);
+        }, 0)
+        return total / subjects.length || 0;
+    }
+}
+const student = new Student("Олег Никифоров");
+console.log(student)
+student.addMark(5, "химия");
+student.addMark(5, "химия");
+student.addMark(5, "физика");
+student.addMark(4, "физика");
+student.addMark(6, "физика"); // Оценка не добавится, так как больше 5
+console.log(student.marks)
+console.log(student.getAverageBySubject("физика")); // Средний балл по предмету физика 4.5
+console.log(student.getAverageBySubject("биология")); // Вернёт 0, так как по такому предмету нет никаких оценок.
+console.log(student.getAverageBySubject("химия"));
+console.log(student.getAverage()); // Средний балл по всем предметам 4.75
