9abe465749f6833e3d1a4d37a6edc96605bc441a
Vyacheslav <slavik.kurganov.26@mail.ru>
2025-10-08 17:10:16 +0300

    changed task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,31 +1,43 @@
 function Student(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.marks = [];
-};
-
-const studentMaha = new Student ('Maha', 'Женский', 20);
-const studentBob = new Student ('Bob', 'Мужской', 22);
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.marks = [];
+}
 
 Student.prototype.setSubject = function (subjectName) {
-    this.subject = subjectName;
+    return this.subject = subjectName;
 };
 
-Student.prototype.addMarks = function (...marksToAdd) {
-    if (this?.marks) {
-    this.marks.push(...marksToAdd);
-    };
+Student.prototype.addMarks = function (...marksToAdd){
+    if (this.marks) {
+        this.marks.push(...marksToAdd);
+    }
 };
 
-Student.prototype.getAverage = function () {
-    if (this.marks?.length > 0 ) {
-    return this.marks.reduce((a, b) => a + b) / this.marks.length;
-    }else return 0;
+Student.prototype.getAverage = function getAverage() {
+    if (!this.marks || this.marks.length === 0 ) {
+        return 0;
+    }else {
+        return this.marks.reduce((sum, item) => sum + item) / this.marks.length;
+    }
 };
 
-Student.prototype.exclude = function (reason) {
+Student.prototype.exclude = function exclude(reason) {
     delete this.subject;
     delete this.marks;
-    this.excluded = reason;
-};
+    return this.excluded = reason;
+}
+
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
+// {age: 19, gender: "женский", marks: [4, 5, 4, 5], name: "Василиса", subject: "Algebra"}
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба')
+console.log(student2)
+// {name: "Артём", gender: "мужской", age: 25, excluded: "плохая учёба"}
